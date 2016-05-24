d227fb399a2607006c3c9157c44c0d979ceb3d11
Satya Mallick <spmallick@gmail.com>
2016-05-24 12:45:44 -0700

    variable name change and some optimizations

--- examples/webcam_face_pose_fast.cpp
+++ examples/webcam_face_pose_fast.cpp
@@ -53,11 +53,13 @@ int main()
             return 1;
         }
 
-        cv::Mat temp;
-        cap >> temp;
-        cv::Mat temp_small, temp_small2;
-        cv::resize(temp, temp_small, cv::Size(), 1.0/FACE_DOWNSAMPLE_RATIO, 1.0/FACE_DOWNSAMPLE_RATIO);
-        cv::resize(temp, temp_small2, cv::Size(), 0.5, 0.5);
+        cv::Mat im;
+        
+        // Get first frame and allocate memory.
+        cap >> im;
+        cv::Mat im_small, im_display;
+        cv::resize(im, im_small, cv::Size(), 1.0/FACE_DOWNSAMPLE_RATIO, 1.0/FACE_DOWNSAMPLE_RATIO);
+        cv::resize(im, im_display, cv::Size(), 0.5, 0.5);
 
         
         
@@ -84,18 +86,14 @@ int main()
             if ( count == 0 )
                 t = cv::getTickCount();
             // Grab a frame
-            cap >> temp;
-            // Turn OpenCV's Mat into something dlib can deal with.  Note that this just
-            // wraps the Mat object, it doesn't copy anything.  So cimg is only valid as
-            // long as temp is valid.  Also don't do anything to temp that would cause it
-            // to reallocate the memory which stores the image as that will make cimg
-            // contain dangling pointers.  This basically means you shouldn't modify temp
-            // while using cimg.
+            cap >> im;
             
-
-            cv::resize(temp, temp_small, cv::Size(), 1.0/FACE_DOWNSAMPLE_RATIO, 1.0/FACE_DOWNSAMPLE_RATIO);
-            cv_image<bgr_pixel> cimg_small(temp_small);
-            cv_image<bgr_pixel> cimg(temp);
+            // Resize image for face detection
+            cv::resize(im, im_small, cv::Size(), 1.0/FACE_DOWNSAMPLE_RATIO, 1.0/FACE_DOWNSAMPLE_RATIO);
+            
+            // Change to dlib's image format. No memory is copied.
+            cv_image<bgr_pixel> cimg_small(im_small);
+            cv_image<bgr_pixel> cimg(im);
             
 
             // Detect faces 
@@ -119,7 +117,7 @@ int main()
                 full_object_detection shape = pose_model(cimg, r);
                 shapes.push_back(shape);
 #ifdef OPENCV_FACE_RENDER
-                    render_face(temp, shape);
+                    render_face(im, shape);
 #endif
             }
             
@@ -127,16 +125,23 @@ int main()
             // Display it all on the screen
 #ifdef OPENCV_FACE_RENDER
             
-                cv::resize(temp, temp_small2, cv::Size(), 0.5, 0.5);
-                cv::imshow("image", temp_small2);
-                // waitKey slows down the runtime quite a lot
-                /*
-                int k = cv::waitKey(1);
-                if ( k == 'q')
+                // Resize image for display
+                cv::resize(im, im_display, cv::Size(), 0.5, 0.5);
+                cv::imshow("image", im_display);
+            
+                // WaitKey slows down the runtime quite a lot
+                // So check every 15 frames
+            
+                if ( count % 15 == 0)
                 {
-                    return 0;
+                    int k = cv::waitKey(1);
+                    // Quit if 'q' or ESC is pressed
+                    if ( k == 'q' || k == 27)
+                    {
+                        return 0;
+                    }
                 }
-                 */
+            
  
 #else
  
